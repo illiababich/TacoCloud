e06dd89a893d7e36bc8c02dc2ee80a8de1415f89
illiababich <illia.babich.2@gmail.com>
2022-10-26 23:24:35 +0300

    continued working on implementing REST

--- src/main/java/tacocloud/controllers/api/TacoController.java
+++ src/main/java/tacocloud/controllers/api/TacoController.java
@@ -2,17 +2,18 @@ package tacocloud.controllers.api;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import tacocloud.Taco;
 import tacocloud.data.TacoRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/tacos", produces = "application/json")
 @CrossOrigin(origins = "http://tacocloud:8080")
-//https://localhost:8443/api/tacos?recent to get the json of development config
+// https://localhost:8443/api/tacos?recent to get the json of development config
 public class TacoController {
     private final TacoRepository tacoRepository;
 
@@ -24,5 +25,20 @@ public class TacoController {
     public Iterable<Taco> recentTacos() {
         PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
         return tacoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optionalTaco = tacoRepository.findById(id);
+        if (optionalTaco.isPresent()) {
+            return new ResponseEntity<>(optionalTaco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepository.save(taco);
     }
 }
